5eba2012e928f3ac8762ebfc46b8c4c2ba6f05e2
Alex <kovalyshyn.alex@gmail.com>
2018-01-10 11:14:43 +0200

    made UI

---
+++ CryptoCoin/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  CryptoCoin
+//
+//  Created by admin on 10.01.2018.
+//  Copyright © 2018 skovcustom. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
