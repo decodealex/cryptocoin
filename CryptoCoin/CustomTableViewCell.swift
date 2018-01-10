43a9ad0655dbe475f910aff08ba0fdfa533f9c9e
Alex <kovalyshyn.alex@gmail.com>
2018-01-10 11:19:49 +0200

    addOutlets

--- CryptoCoin/CustomTableViewCell.swift
+++ CryptoCoin/CustomTableViewCell.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var cellView: UIView!
+    
+    @IBOutlet weak var coinImage: UIImageView!
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    
+    @IBOutlet weak var smallNameLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var changeLabel: UILabel!
+    
+    @IBOutlet weak var arrowImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
