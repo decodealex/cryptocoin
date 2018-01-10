6209dc0dc9ecbf07621cd2736c2a29e1152e768a
Alex <kovalyshyn.alex@gmail.com>
2018-01-10 13:20:36 +0200

    ivan json

--- CryptoCoin/ViewController.swift
+++ CryptoCoin/ViewController.swift
@@ -7,26 +7,35 @@
 //
 
 import UIKit
+import Foundation
+
+class Crypt: Codable {
+    var id: String
+    var name: String
+}
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var crypts: [Crypt] = []
+    
     var coinFullNames = ["Bitcoin", "Ethereum", "Ripple", "Bitcoin Cash", "Cardano"]
     
+    var changeValue = ["-9.26",  "-9.26", "-9.26", "-9.26", "-9.26",]
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
         
-        
-        
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.loadData()                        
     }
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return coinFullNames.count
+//        return coinFullNames.count
+        return self.crypts.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -35,16 +44,56 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        let crypt = self.crypts[indexPath.row]
         
         cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
         
-        cell.fullNameLabel.text = coinFullNames[indexPath.row]
-        cell.coinImage.image = UIImage(named: coinFullNames[indexPath.row])
-        cell.coinImage.layer.cornerRadius = cell.coinImage.frame.height / 2
+        cell.fullNameLabel.text = crypt.name
+//        cell.coinImage.image = UIImage(named: coinFullNames[indexPath.row])
+//        cell.coinImage.layer.cornerRadius = cell.coinImage.frame.height / 2
+//        cell.changeLabel.text
+//        cell.changeLabel.text = changeValue[indexPath.row]
+        
+        
+        cell.changeLabel.textColor = .red
+        
         
         return cell
     }
 
+    func loadData() {
+        
+        let url = URL(string: "https://api.coinmarketcap.com/v1/ticker/")!
+//        var requst = URLRequest(url: url)
+//        requst.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            
+            print(response)
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                
+                let decoder = JSONDecoder()
+                let crypts = try decoder.decode([Crypt].self, from: data)
+                
+                self.crypts = crypts
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            } catch {
+                
+            }
+        })
+        
+    }
+    
 }
 
