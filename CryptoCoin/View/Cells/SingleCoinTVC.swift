75ce404b0506129dce768a98ad1ca7c0ec78a005
Alex <kovalyshyn.alex@gmail.com>
2018-03-23 11:03:41 +0200

    cryptoCoin
    master commits

---
+++ CryptoCoin/View/Cells/SingleCoinTVC.swift
@@ -0,0 +1,20 @@
+//
+//  SingleCoinTVC.swift
+//  CryptoCoin
+//
+//  Created by admin on 17.01.2018.
+//  Copyright © 2018 skovcustom. All rights reserved.
+//
+
+import UIKit
+
+class SingleCoinTVC: UITableViewCell {
+
+    @IBOutlet weak var singleCoinImg: UIImageView!
+    
+    @IBOutlet weak var change1hour: UILabel!
+    
+    @IBOutlet weak var change24hour: UILabel!
+    
+    @IBOutlet weak var change7days: UILabel!
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CryptoCoin' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CryptoCoin
+
+	pod 'Alamofire', '~> 4.6'
+
+	pod 'Cache', '~> 4.1'
+end
